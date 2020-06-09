517d4e7213d01bb678f43b6a52f1e6cfe1388bfb
YatinKumarGupta <66680134+YatinKumarGupta@users.noreply.github.com>
2020-06-09 23:47:54 +0530

    Update Branch.v

--- Branch.v
+++ Branch.v
@@ -29,20 +29,20 @@ output  reg br;
 reg [2:0] x;
 always@(*)
 begin
-	if(ALUOp[1] & ALUOp[0])
+	if(ALUOp[1] & ALUOp[0]) 		//Whenever AND operation of both bits of ALUOp is high 
 	begin 
-		br_pc = im_addr;
-		br = 1;
+		br_pc = im_addr;   		//Immediate address will go to br_pc variable
+		br = 1; 			// 'br'(select line in Fetch stage) goes high
 	end
 	else
 	begin
-		br_pc = im_ext + pc;
-		if(ALU_ctrl == 4'd10)
-			br = flag[1] & br_ctrl;
-		else if(ALU_ctrl == 4'd11)
-			br = flag[0] & br_ctrl;
-		else
-			br = 0;
+		br_pc = im_ext + pc;   		// if above condition fails then, immediate extended value is added to PC and assigned to br_pc
+	if(ALU_ctrl == 4'd10) 			//when ALU_ctrl is 10(decimal)
+		br = flag[1] & br_ctrl;  	//we check whether there is a branching instruction or not and also carry is generating or not. if yes, then br goes high
+	else if(ALU_ctrl == 4'd11) 		// if ALU_ctrl has a value 11(dec) then,
+		br = flag[0] & br_ctrl; 	// again checking if instruction is a branch and also zero flag is generating or not, if yes, then br goes high
+	else
+		br = 0;				//If nothing satisfies then 'br' remains low.
 	end 
 end
 endmodule
