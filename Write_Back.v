72140a2c11c540d86a097b0d9fb3851f48c5149d
YatinKumarGupta <66680134+YatinKumarGupta@users.noreply.github.com>
2020-06-09 23:54:00 +0530

    Update Write_Back.v

--- Write_Back.v
+++ Write_Back.v
@@ -23,9 +23,9 @@ input [31:0] Read_data,ALU_result;
 input [8:0] ctrl_sig;
 output reg[31:0] Write_Back;
 always@(*)
-	if(ctrl_sig[6])
-		Write_Back = Read_data;
+	if(ctrl_sig[6]) // If MemtoReg goes high
+		Write_Back = Read_data;  // read_data is written back to register bank
 	else
-		Write_Back = ALU_result;
+		Write_Back = ALU_result; // otherwise result from ALU unit is written back to register bank
 
 endmodule
