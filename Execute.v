f45c14fde8aaf22a3d5fa2b66b476d489717e7cc
bumblebee17 <56111886+bumblebee17@users.noreply.github.com>
2020-06-09 23:36:44 +0530

    Update Execute.v

--- Execute.v
+++ Execute.v
@@ -30,13 +30,11 @@ output br;
 wire [1:0] flag,ForA,ForB;
 wire [3:0] ALU_ctrl;
 wire [31:0] r2,x1,x2;
-Forwading_unit F1(mem_rd,wb_rd,rs,rt,ForA,ForB);
-//Forwading_unit(mem_rd,wb_rd,rs,rt,ForA,ForB);
-mux_4x1 m1(Read_data1,mem_wb_data,ex_mem_data,ForA,x1);
-mux_4x1 m2(Read_data2,mem_wb_data,ex_mem_data,ForB,x2);
+Forwading_unit F1(mem_rd,wb_rd,rs,rt,ForA,ForB); // Forwading Unit
+mux_4x1 m1(Read_data1,mem_wb_data,ex_mem_data,ForA,x1); // Mux for selecting the first operand 
+mux_4x1 m2(Read_data2,mem_wb_data,ex_mem_data,ForB,x2); // Mux for selecting the second operand 
 ALU_control A1(funcy,ctrl_sig[1:0],ALU_ctrl);
-assign r2 = ctrl_sig[7] ? im_ext : x2;
+assign r2 = ctrl_sig[7] ? im_ext : x2;  
 ALU A2(ALU_ctrl,x1,r2,alu_result,flag,cmp);
-Branch B1(ALU_ctrl,im_addr1,im_ext,pc_exe,ctrl_sig[2],{ctrl_sig[1],ctrl_sig[0]},flag,br_pc,br);
-//Branch(ALU_ctrl,im_addr,im_ext,pc,br_ctrl,ALUOp,flag,br_pc,br);
+  Branch B1(ALU_ctrl,im_addr1,im_ext,pc_exe,ctrl_sig[2],{ctrl_sig[1],ctrl_sig[0]},flag,br_pc,br); 
 endmodule
